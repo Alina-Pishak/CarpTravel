26f15861125b8b7c777589717feb1776a559af82
Alina-Pishak <alinapishak@icloud.com>
2024-04-16 21:07:11 +0300

    configure basic settings, add data

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        hero: "url('/img/hero-bg-1x.jpg')",
+      },
+    },
+    colors: {
+      primary: "fff",
+    },
+  },
+  plugins: [],
+};
+export default config;
